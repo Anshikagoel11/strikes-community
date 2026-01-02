c3759062e3fe440b8a1ce83ecc84e7e5e9a032fb
Arbaz Ansari <mail.arbazansari@gmail.com>
2026-01-02 15:53:36 +0530

    clerk config

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+
+export default async function Home() {
+  const { isAuthenticated } = await auth();
   return (
+
     <div>
-      <h1>this is a protected route</h1>
+      {/* Navbar */}
+      <nav className="flex justify-between bg-gray-300 items-center">
+        <div>Discord</div>
+        <div className="flex items-center">
+          {isAuthenticated ?
+            <SignedIn>
+              <Link href={"/dashboard"}><Button>Dashboard</Button></Link>
+              <UserButton />
+            </SignedIn>
+            :
+            <SignedOut>
+              <Link href={"/sign-in"}><Button>Signin</Button></Link>
+              <Link href={"/sign-up"}><Button>Signup</Button></Link>
+            </SignedOut>
+          }
+        </div>
+      </nav>
+      <h1>home page</h1>
     </div>
   );
 }
