7807d81096c0d0026f284f7a79e973b1832238ef
Arbaz Ansari <mail.arbazansari@gmail.com>
2026-01-09 15:43:02 +0530

    input ui update
    Co-Authored-By: Anshika Goel <168545874+Anshikagoel11@users.noreply.github.com>
    Co-Authored-By: DEVEN WAGH <wagh1.2.3.002@gmail.com>

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import z from "zod"
 import { Form, FormControl, FormField, FormItem } from '../ui/form'
-import { Plus } from 'lucide-react'
+import { Plus, Send, Loader2 } from 'lucide-react'
 import qs from "query-string"
 import axios from 'axios'
 import { useModal } from '@/hooks/use-mode-store'
@@ -54,13 +54,23 @@ const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                 <FormField control={form.control} name='content' render={({ field }) => (
                     <FormItem className='w-full'>
                         <FormControl>
-                            <div className='flex items-center gap-2 w-full'>
-                                <button type='button' className='h-10 w-10 rounded-md bg-card flex items-center justify-center hover:bg-zinc-200 dark:hover:bg-zinc-700 transition' onClick={() => onOpen("messageFile", { apiUrl, query })}>
+                            <div className='relative flex items-center gap-2 w-full'>
+                                <button type='button' className='h-10 w-10 rounded-md bg-card flex items-center justify-center hover:bg-zinc-200 dark:hover:bg-zinc-700 transition border' onClick={() => onOpen("messageFile", { apiUrl, query })}>
                                     <Plus className='text-zinc-500 dark:text-zinc-400' />
                                 </button>
-                                <input type="text" className='bg-card p-2 rounded-md relative w-full' placeholder={`Message ${type === "conversation" ? name : "#" + name}`} {...field} disabled={isLoading} autoComplete="off" />
+                                <input type="text" className='bg-card p-2 rounded-md h-10 w-full text-zinc-600 dark:text-zinc-200 border' placeholder={`Message ${type === "conversation" ? name : "#" + name}`} {...field} disabled={isLoading} autoComplete="off" />
+                                <button
+                                    type="submit"
+                                    disabled={isLoading}
+                                    className='absolute h-8 w-8 right-13 flex items-center justify-center rounded-md cursor-pointer hover:bg-zinc-300/10 transition text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300'
+                                >
+                                    {isLoading ? (
+                                        <Loader2 className='h-5 w-5 animate-spin' />
+                                    ) : (
+                                        <Send className='h-5 w-5' />
+                                    )}
+                                </button>
                                 <EmojiPicker onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)} />
-
                             </div>
                         </FormControl>
                     </FormItem>
